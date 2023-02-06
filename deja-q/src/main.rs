45599ca6f789d6c846602d08daff52a19f437fab
Elijah Mirecki <elimirks@gmail.com>
2023-02-05 23:01:10 -0500

    Massage code to make pacman AIs easier to add

--- deja-q/src/main.rs
+++ deja-q/src/main.rs
@@ -1,19 +1,19 @@
-/*
- * G--
- * -PG
- * ---
- */
-
 use std::{thread::sleep, time::Duration};
+use rand::prelude::SliceRandom;
 
-use rand::{prelude::SliceRandom, Rng};
-
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 enum Tile {
-    Pacman,
-    Ghost,
     Empty,
     Wall,
+}
+
+#[derive(Copy, Clone, PartialEq)]
+enum Direction {
+    Noop,
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
 fn clear_screen() {
@@ -22,12 +22,20 @@ fn clear_screen() {
 
 struct Board {
     tiles: Vec<Vec<Tile>>,
+    ghosts: Vec<(usize, usize)>,
+    pacman: (usize, usize),
+}
+
+trait PacmanAi {
+    fn select_move(&mut self, surroundings: [Tile; 8]) -> Direction;
 }
 
 impl Board {
     fn new(width: usize, height: usize) -> Self {
         Board {
             tiles: vec![vec![Tile::Empty; width]; height],
+            ghosts: vec![],
+            pacman: (5, 5),
         }
     }
 
@@ -46,62 +54,77 @@ impl Board {
 
     fn render(&self) {
         clear_screen();
-        for row in self.tiles.iter() {
-            for tile in row {
-                print!(
-                    "{}",
-                    match tile {
-                        Tile::Pacman => '🤪',
-                        Tile::Ghost => '👻',
-                        Tile::Empty => '　',
-                        Tile::Wall => '⬜',
-                    }
-                );
+        for y in 0..self.tiles.len() {
+            for x in 0..self.tiles[0].len() {
+                print!("{}", match self.tiles[y][x] {
+                    Tile::Empty => {
+                        if self.pacman == (x, y) {
+                            '🤪'
+                        } else if self.ghosts.contains(&(x, y)) {
+                            '👻'
+                        } else {
+                            '　'
+                        }
+                    },
+                    Tile::Wall => '⬜',
+                });
             }
             println!();
         }
     }
 
-    fn tick(&mut self) {
-        let ghost_points = self
-            .tiles
-            .iter()
-            .enumerate()
-            .flat_map(|(y, row)| {
-                row.iter()
-                    .enumerate()
-                    .filter_map(move |(x, tile)| match tile {
-                        Tile::Ghost => Some((x, y)),
-                        _ => None,
-                    })
-            })
-            .collect::<Vec<_>>();
-        let mut rng = rand::thread_rng();
-        for (x, y) in ghost_points.iter() {
-            let available_moves = [(*x, *y - 1), (*x, *y + 1), (*x - 1, *y), (*x + 1, *y)]
-                .into_iter()
-                .filter(|(x, y)| matches!(self.tiles[*y][*x], Tile::Empty | Tile::Pacman))
-                .collect::<Vec<_>>();
+    fn has_ghost_at(&self, x: usize, y: usize) -> bool {
+        self.ghosts.contains(&(x, y))
+    }
 
+    fn tick_ghosts(&mut self) {
+        let mut rng = rand::thread_rng();
+        for i in 0..self.ghosts.len() {
+            let (x, y) = self.ghosts[i];
+            let available_moves = [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]
+                .into_iter()
+                .filter(|(x, y)| matches!(self.tiles[*y][*x], Tile::Empty) && !self.has_ghost_at(*x, *y))
+                .collect::<Vec<_>>();
             if let Some((xp, yp)) = available_moves.choose(&mut rng) {
-                self.tiles[*y][*x] = Tile::Empty;
-                self.tiles[*yp][*xp] = Tile::Ghost;
+                self.ghosts[i] = (*xp, *yp);
             }
         }
+    }
+
+    fn is_game_over(&self) -> bool {
+        self.ghosts.contains(&self.pacman)
+    }
+}
+
+struct QLearningPacman {
+}
+
+impl QLearningPacman {
+    fn new() -> Self {
+        QLearningPacman {}
+    }
+}
+
+impl PacmanAi for QLearningPacman {
+    fn select_move(&mut self, surroundings: [Tile; 8]) -> Direction {
+        todo!()
     }
 }
 
 fn main() {
     let mut board = Board::empty_with_edge_walls(10, 10);
     for x in 1..5 {
-        board.tiles[x][1] = Tile::Ghost;
-        board.tiles[x][2] = Tile::Ghost;
-        board.tiles[x][3] = Tile::Ghost;
+        board.ghosts.push((x, 1));
+        board.ghosts.push((x, 2));
+        board.ghosts.push((x, 3));
     }
-    board.tiles[5][5] = Tile::Pacman;
     loop {
         board.render();
         sleep(Duration::from_millis(100));
-        board.tick();
+        board.tick_ghosts();
+        if board.is_game_over() {
+            println!("Game over... LOSER");
+            break;
+        }
     }
 }
